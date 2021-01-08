3cb153d1b772daac5eb71d59457622856fc25c78
Thomas Sostarics <tsostarics@gmail.com>
2021-01-07 20:46:24 -0600

    add multi-presentation permutation functionalility

--- R/add_stimuli_by.R
+++ R/add_stimuli_by.R
@@ -1,6 +1,6 @@
 #' Add Stimuli by Manipulations
 #'
-#' @param .data Design
+#' @param design Design
 #' @param ... A series of formulas where the left hand side is a manipulation
 #' and the right hand side is a series of variable names
 #'
@@ -15,7 +15,7 @@
 #' add_trials(critical = 12, filler = 8) %>%
 #' add_stimuli_by(context ~ preamble_text + question_text, contour ~ audio_file)
 #'
-add_stimuli_by <- function(.data, ...){
+add_stimuli_by <- function(design, ...){
   # a ~ b + c becomes c('~', 'a', 'b + c') w/ as.character
   forms <- lapply(enexprs(...), as.character)
 
@@ -23,8 +23,8 @@ add_stimuli_by <- function(.data, ...){
     lmap(forms,
          function(x){
            x <- x[[1L]]
-           trial_n <- attr(.data[['trials']], 'total')
-           stim_table <- select(get_stim_table(.data), -id)
+           trial_n <- attr(design[['trials']], 'total')
+           stim_table <- select(get_stim_table(design), -id)
 
            # If this stimulus doesn't vary by any manipulation ( ~ x)
            # (eg for a preamble that always appears)
@@ -39,13 +39,26 @@ add_stimuli_by <- function(.data, ...){
              # For stimuli that vary by a manipulation (manip ~ x + y + z...)
              ## Get the manipulation name, cross by number of trials, set name
              f_manip <- x[[2L]] # Manipulation from lhs of formula
-             expanded <- expand.grid(.data[['manipulations']][[f_manip]],
-                                     trial = 1:trial_n)
-             names(expanded)[1L] <- f_manip
+
 
              ## Get columns to add from rhs of formula
              add_cols <- strsplit(as.character(x[[3L]]), " \\+ ")[[1L]]
+             ordering <- attr(design[['manipulations']][[f_manip]], 'ordering')
+
+             if (!isFALSE(ordering)) {
+               order_nums <- 1:length(ordering)
+               add_cols <-
+                 as.vector(
+                   sapply(add_cols,
+                          FUN = function(x) paste(x, order_nums, sep = "_")
+                   )
+                 )
+             }
+
+             expanded <- .expand_manipulation(design, f_manip, trial_n)
+
              add_cols <- lmap(add_cols, function(x) setNames(list(NA), x))
+             print(add_cols)
 
              output <-
                left_join(expanded, stim_table) %>%
@@ -57,6 +70,25 @@ add_stimuli_by <- function(.data, ...){
          }
     )
   # Add stimulus skeletons to our design
-  .data[['stimuli']] <- skeleton_list
-  .data
+  design[['stimuli']] <- skeleton_list
+  design
+}
+
+#' @importFrom gtools permutations
+.expand_manipulation <- function(design, manip, trials_n){
+  ordered_manips <-  vapply(design[['manipulations']],
+                            FUN = function(x)
+                              !isFALSE(attr(x,'ordering')),
+                            FUN.VALUE = TRUE)
+  if (ordered_manips[manip]) {
+    ordering <- attr(design[['manipulations']][[manip]], 'ordering')
+    ordering <- mutate(ordering, set = row_number())
+    trials_df <- expand.grid(trial = 1:trials_n, set = 1:nrow(ordering))
+    expanded <- left_join(trials_df, ordering, by = 'set')
+  }
+  else{
+    expanded <- expand.grid(design[['manipulations']][[manip]], trial = 1:trials_n)
+    names(expanded)[1L] <- manip
+  }
+  expanded
 }
---
+++ R/present_n_of.R
@@ -0,0 +1,35 @@
+#' Present n of the conditions of a manipulation
+#'
+#' @param design Experimental design
+#' @param manipulation Manipulation, can be quoted or unquoted
+#' @param n Number to present
+#'
+#' @return
+#' @export
+#' @importFrom gtools permutations
+#' @examples
+present_n_of <- function(design, manipulation, n){
+  # Enable unquoted argument for ease
+  manipulation <- as.character(enexpr(manipulation))
+
+  if (!manipulation %in% names(design[['manipulations']]))
+    stop(paste0("manipulation ",manipulation," not found in current design"))
+  if (n > length(design[['manipulations']][[manipulation]]))
+    stop("n is greater than the number of conditions for this manipulation")
+
+  # Get the conditions for the manipulation
+  conditions <- design[['manipulations']][[manipulation]]
+
+  # Get a matrix with each permutation of "condition pick n"
+  perm_matrix <- data.frame(permutations(length(conditions), r = n, conditions))
+
+  # Set names of each column in the matrix to manipulation_1...n
+  choice_names <- vapply(1:n,
+                         FUN = function(x) paste(manipulation, x, sep = "_"),
+                         FUN.VALUE = 'character')
+  names(perm_matrix) <- choice_names
+
+  # Retain the original info of the manipulation, but set the new ordering
+  attr(design[['manipulations']][[manipulation]], 'ordering') <- perm_matrix
+  design
+}
