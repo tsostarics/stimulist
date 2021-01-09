201d645f36cdaa28b8439ea9a4013583e1680124
Thomas Sostarics <tsostarics@gmail.com>
2021-01-09 16:24:18 -0600

    Tidy up the function, added some helpers

--- R/add_stimuli_by.R
+++ R/add_stimuli_by.R
@@ -17,81 +17,107 @@
 #'
 add_stimuli_by <- function(design, ...){
   # a ~ b + c becomes c('~', 'a', 'b + c') w/ as.character
-  forms <- lapply(enexprs(...), as.character)
+  formulas <- lapply(enexprs(...), as.character)
+  n_trials <- attr(design[['trials']], 'total')
+  stim_table <- select(get_stim_table(design), -id)
+  print(formulas)
+  for (stimset in formulas) {
 
-  skeleton_list <-
-    lmap(forms,
-         function(x){
-           x <- x[[1L]]
-           trial_n <- attr(design[['trials']], 'total')
-           stim_table <- select(get_stim_table(design), -id)
+    # If this stimulus doesn't vary by any manipulation ( ~ x) then just add
+    # the columns with the columns in the stimulus table and add to the design
+    if (length(stimset) == 2) {
+      f_cols <- strsplit(as.character(stimset[[2L]]), " \\+ ")[[1L]]
+      add_cols <- lmap(f_cols, function(x) setNames(list(NA), x))
+      output <- cbind(data.frame(stim_table), add_cols)
+      design[['stimuli']][['constant_for_all']] <- output
+      design[['presentations']][['constant_for_all']] <- output
+    }
+    else{
+      # For stimuli that vary by a manipulation (manip ~ x + y + z...)
+      # Get the manipulation name, cross by number of trials, set name
+      f_manip <- stimset[[2L]] # Manipulation from lhs of formula
 
-           # If this stimulus doesn't vary by any manipulation ( ~ x)
-           # (eg for a preamble that always appears)
-           if (length(x) == 2) {
-             add_cols <- strsplit(as.character(x[[2L]]), " \\+ ")[[1L]]
-             add_cols <- lmap(add_cols, function(x) setNames(list(NA), x))
-             output <- list(cbind(data.frame(stim_table), add_cols))
+      ## Get columns to add from rhs of formula
+      f_cols <- strsplit(as.character(stimset[[3L]]), " \\+ ")[[1L]]
 
-             setNames(output, 'constant_for_all')
-           }
-           else{
-             # For stimuli that vary by a manipulation (manip ~ x + y + z...)
-             ## Get the manipulation name, cross by number of trials, set name
-             f_manip <- x[[2L]] # Manipulation from lhs of formula
+      # Cross manipulation conditions with number of trials
+      manipulation <- design[['manipulations']][[f_manip]]
+      expanded <- expand.grid(manipulation, trial = 1:n_trials)
+      names(expanded)[1L] <- f_manip # expand.grid first column name is `Var1`
 
+      # Make empty singleton columns to bind to output
+      add_cols <- set_names(as.list(rep(NA, length(f_cols))), f_cols)
 
-             ## Get columns to add from rhs of formula
-             add_cols <- strsplit(as.character(x[[3L]]), " \\+ ")[[1L]]
-             has_order <- attr(design[['manipulations']][[f_manip]], 'has_order')
+      output <-
+        left_join(expanded, stim_table, by = 'trial') %>%
+        relocate(trial, type, stimulus) %>%
+        cbind(add_cols)
 
-             if (has_order) {
-               ordering <- design[['orderings']][[f_manip]]
-               order_nums <- 1:length(ordering)
-               add_cols <-
-                 as.vector(
-                   sapply(add_cols,
-                          FUN = function(x) paste(x, order_nums, sep = "_")
-                   )
-                 )
-             }
+      # If there's an ordering, then make a presentation using the different
+      # permutations of that condition. Otherwise the presentation is the same
+      # as the stimulus set.
+      has_order <- attr(manipulation, 'has_order')
+      if (has_order)
+        design[['presentations']][[f_manip]] <- .make_presentation(design,
+                                                                   f_manip,
+                                                                   n_trials,
+                                                                   f_cols,
+                                                                   stim_table)
+      else
+        design[['presentations']][[f_manip]] <- output
 
-             expanded <- .expand_manipulation(design, f_manip, trial_n)
-
-             add_cols <- lmap(add_cols, function(x) setNames(list(NA), x))
-             print(add_cols)
-
-             output <-
-               left_join(expanded, stim_table) %>%
-               relocate(trial, type, stimulus) %>%
-               cbind(add_cols)
-
-             output <- setNames(list(output), f_manip)
-             output
-           }
-         }
-    )
-  # Add stimulus skeletons to our design
-  print(skeleton_list)
-  design[['stimuli']] <- skeleton_list
+      design[['stimuli']][[f_manip]] <- output
+    }
+  }
   design
 }
 
+.clean_formulas <- function(formulas){
+  # note: need to add a helper function that merges formulas when they
+  # address the same manipulation. eg in add_stimuli_by(~one, ~two), two
+  # will overwrite one rather unlike in add_stimuli_by(~one + two)
+}
+
 #' @importFrom gtools permutations
-.expand_manipulation <- function(design, manip, trials_n){
+.expand_ordering <- function(design, manipulation, n){
+
+  # Get which manipulations have orderings
   ordered_manips <-  vapply(design[['manipulations']],
                             FUN = function(x)
                               attr(x,'has_order'),
                             FUN.VALUE = TRUE)
-  if (ordered_manips[manip]) {
-    ordering <- design[['orderings']][[manip]]
+  if (ordered_manips[manipulation]) {
+    ordering <- design[['orderings']][[manipulation]]
     ordering <- mutate(ordering, set = row_number())
-    trials_df <- expand.grid(trial = 1:trials_n, set = 1:nrow(ordering))
+    trials_df <- expand.grid(trial = 1:n, set = 1:nrow(ordering))
     expanded <- left_join(trials_df, ordering, by = 'set')
   }
   else{
-    expanded <- expand.grid(design[['manipulations']][[manip]], trial = 1:trials_n)
-    names(expanded)[1L] <- manip
+    expanded <- expand.grid(design[['manipulations']][[manipulation]],
+                            trial = 1:n)
+    names(expanded)[1L] <- manipulation
   }
   expanded
+}
+
+.make_presentation <- function(design, manipulation, n, columns, stimulus_table){
+  ordering <- design[['orderings']][[manipulation]]
+  order_nums <- 1:length(ordering)
+  add_cols <-
+    as.vector(
+      sapply(columns,
+             FUN = function(x) paste(x, order_nums, sep = "_")
+      )
+    )
+
+  expanded <- .expand_ordering(design, manipulation, n)
+
+  add_cols <- set_names(as.list(rep(NA, length(columns))), columns)
+
+  output <-
+    left_join(expanded, stimulus_table, by = 'trial') %>%
+    relocate(trial, type, stimulus) %>%
+    cbind(add_cols)
+
+  output
 }
